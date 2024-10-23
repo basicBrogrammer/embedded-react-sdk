22dd170f15f4a992c09ca2295d187fb9360ef014
Sean Scally <scally@users.noreply.github.com>
2024-10-23 10:49:34 -0700

    fix: remove buildkite for now (#3)
    * fix: remove buildkite for now
    
    * add lint/format to ci
    
    * fix: use formatter
    
    * fix: test:ci not ci:test

--- test/mocks/fileMock.js
+++ test/mocks/fileMock.js
@@ -2,6 +2,6 @@ export default {
   process() {
     return {
       code: `module.exports = {__esModule: true, default: "test-file-stub"}`,
-    };
+    }
   },
-};
+}
--- test/mocks/styleMock.js
+++ test/mocks/styleMock.js
@@ -1,8 +1,8 @@
-import path from 'path';
+import path from 'path'
 export default {
   process(sourceText, sourcePath) {
     return {
       code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
-    };
+    }
   },
-};
+}
