982415863d3fd81ef36814cf60853d3fe8897223
Aaron Lee <aaron.lee@gusto.com>
2025-04-07 17:01:05 -0400

    style: add skeleton loading to component loader UI (#187)
    * style: add skeleton loading to component loader UI
    
    * style: use proper classnames package
    
    * style: fix linter error flagging redundant null checking
    
    ---------
    
    Co-authored-by: Jeffrey D Johnson <jeffredodd@users.noreply.github.com>

--- src/components/Common/Loading/Loading.tsx
+++ src/components/Common/Loading/Loading.tsx
@@ -1,10 +1,17 @@
 import { useTranslation } from 'react-i18next'
+import cn from 'classnames'
+import styles from './Loading.module.scss'
 
 export const Loading = () => {
   const { t } = useTranslation('common')
   return (
-    <section aria-live="polite" aria-busy>
-      {t('status.loading')}
+    <section
+      className={styles.skeletonContainer}
+      aria-label={t('status.loading')}
+      aria-live="polite"
+      aria-busy
+    >
+      <div className={cn(styles.skeleton, styles.skeletonBox)}></div>
     </section>
   )
 }
