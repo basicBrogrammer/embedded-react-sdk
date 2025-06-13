89e0b1f6d4bca3e7b6b0babaf99f60517f1c34b0
Steve Jensen <steve.jensen@gusto.com>
2025-06-13 14:28:40 -0600

    chore: upgrade embedded api version to 0.6.4 (#346)

--- src/test/mocks/apis/payrolls.ts
+++ src/test/mocks/apis/payrolls.ts
@@ -1,14 +1,16 @@
 import type { PathParams } from 'msw'
 import { http, HttpResponse } from 'msw'
-import type { GetV1CompaniesCompanyIdPayrollsRequest } from '@gusto/embedded-api/models/operations/getv1companiescompanyidpayrolls'
-import type { PayrollMinimal$Outbound } from '@gusto/embedded-api/models/components/payrollminimal'
+import type {
+  GetV1CompaniesCompanyIdPayrollsRequest,
+  GetV1CompaniesCompanyIdPayrollsResponse,
+} from '@gusto/embedded-api/models/operations/getv1companiescompanyidpayrolls'
 import { getFixture } from '../fixtures/getFixture'
 import { API_BASE_URL } from '@/test/constants'
 
 const getHistoricalPayrolls = http.get<
   PathParams,
   GetV1CompaniesCompanyIdPayrollsRequest,
-  PayrollMinimal$Outbound
+  GetV1CompaniesCompanyIdPayrollsResponse
 >(`${API_BASE_URL}/v1/companies/:company_id/payrolls`, async ({ params }) => {
   const responseFixture = await getFixture(
     'get-v1-companies-company_id-payrolls-processed-payrolls',
